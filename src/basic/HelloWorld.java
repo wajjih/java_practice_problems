c523aa82828cbd42315fe49635c7a7c8087595c6
Wajjih Muhammad <57046673+wajjih@users.noreply.github.com>
2019-12-14 15:35:24 -0500

    Completed code

--- src/basic/HelloWorld.java
+++ src/basic/HelloWorld.java
@@ -2,7 +2,9 @@ package basic;
 
 public class HelloWorld {
   public static void main(String[] args) throws Exception {
+    String newLine = System.getProperty("line.separator");     
+    System.out.println("HelloWorld" + newLine + "Wajjih Muhammad");
     // Write a Java program to print 'Hello' on screen and then print your name on a separate line.
-
+//Wajjih Muhammad 
   }
 }
--- src/basic/MathOperators.java
+++ src/basic/MathOperators.java
@@ -2,6 +2,8 @@ package basic;
 
 public class MathOperators {
   public static void main(String[] args) throws Exception {
+    int e = 74 + 36;
+    
     // 1. Write a Java program to print the sum of two numbers.
     /*
     Test Data:
@@ -9,7 +11,7 @@ public class MathOperators {
     Expected Output :
       110
     */
-
+int f = 32/2;
     // 2. Write a Java program to divide two numbers and print on the screen.
     /*
     Test Data :
@@ -19,7 +21,10 @@ public class MathOperators {
     */
 
     // 3. Write a Java program to print the result of the following operations.
-
+int A = -5 + 8 * 6;
+int B = (55+9) % 9;
+int C = 20 + -3 * 5 / 8;
+int D = 5 + 15 / 3 * 2 - 8 % 3;
     /*
     Test Data:
       a. -5 + 8 * 6
@@ -32,5 +37,11 @@ public class MathOperators {
       19
       13
     */
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(A);
+    System.out.println(B);
+    System.out.println(C);
+    System.out.println(D);
   }
 }

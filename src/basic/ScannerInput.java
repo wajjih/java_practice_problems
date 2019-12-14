2ce82dcaaf5579a55a27ed682245e726aaca63b9
Stephon Lawrence <stephonlawrence@gmail.com>
2019-12-14 13:53:00 -0500

    updated scanner input

--- src/basic/ScannerInput.java
+++ src/basic/ScannerInput.java
@@ -17,7 +17,7 @@ public class ScannerInput {
 
 
 
-    
+
 
     // 2. Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
     /*
@@ -31,6 +31,32 @@ public class ScannerInput {
       125 / 24 = 5
       125 mod 24 = 5
     */
+
+
+    // 3. Write a Java program that takes a number as input and prints its multiplication table upto 10.
+    /*
+    Test Data:
+      Input a number: 8
+    Expected Output:
+      8 x 1 = 8
+      8 x 2 = 16
+      8 x 3 = 24
+      ...
+      8 x 10 = 80
+    */
+
+    
+
+    // 4. Write a Java program to display the following pattern.
+    /*
+      Sample Pattern :
+
+   J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a
+    */
+
 
   }
 }

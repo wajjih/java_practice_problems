e7612ef6ef8f58cd2300f6d2282bf4a8b584afbc
Stephon Lawrence <stephonlawrence@gmail.com>
2019-12-14 14:18:26 -0500

    added extra examples to basic

---
+++ src/basic/OtherExamples.java
@@ -0,0 +1,67 @@
+package basic;
+
+public class OtherExamples {
+  public static void main(String[] args) throws Exception {
+    // Write a Java program to compute the specified expressions and print the output.
+    /*
+    Test Data:
+      ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+    Expected Output
+      2.138888888888889
+    */
+
+    // Write a Java program to compute a specified formula.
+    /*
+    Specified Formula :
+    4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+    Expected Output
+    2.9760461760461765
+    */
+
+    // Write a Java program to print the area and perimeter of a circle.
+    /*
+    Test Data:
+    Radius = 7.5
+    Expected Output
+    Perimeter is = 47.12388980384689
+    Area is = 176.71458676442586
+    */
+
+    // Write a Java program that takes three numbers as input to calculate and print the average of the numbers.
+
+
+    // Write a Java program to print the area and perimeter of a rectangle.
+    /*
+    Test Data:
+      Width = 5.5 Height = 8.5
+
+    Expected Output
+      Area is 5.6 * 8.5 = 47.60
+      Perimeter is 2 * (5.6 + 8.5) = 28.20
+    */
+
+    // Write a Java program to print an American flag on the screen.
+    /*
+    Expected Output
+
+* * * * * * ==================================                          
+ * * * * *  ==================================                          
+* * * * * * ==================================                          
+ * * * * *  ==================================                          
+* * * * * * ==================================                          
+ * * * * *  ==================================                          
+* * * * * * ==================================                          
+ * * * * *  ==================================                          
+* * * * * * ==================================                          
+==============================================                          
+==============================================                          
+==============================================                          
+==============================================                          
+==============================================                          
+==============================================
+    */
+
+
+    // Write a Java program to swap two variables.
+  }
+}

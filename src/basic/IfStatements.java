493a3244bb4ed43a2f7e941fb3031b861315bbfb
Stephon Lawrence <stephonlawrence@gmail.com>
2019-12-17 15:48:12 -0500

    added If statements

---
+++ src/basic/IfStatements.java
@@ -0,0 +1,37 @@
+package basic;
+
+public class IfStatements {
+  public static void main(String[] args) throws Exception {
+    // 1. Get number as input, print whether the number is greater than 2
+    /*
+    Test Input:
+      2
+    Expected Output:
+      false
+    */
+
+    // 2. Get number as input, print whether the number is even
+    /*
+    Test Inputs:
+      2
+      3
+    Expected Outputs:
+      true
+      false
+    */
+
+    // 3. Get 2 numbers as input, print whether the 2 numbers are equal to eachother
+    /*
+    Test Inputs:
+      2
+      3
+      ...
+      4
+      4
+    Expected Outputs:
+      false
+      ...
+      true
+    */
+  }
+}

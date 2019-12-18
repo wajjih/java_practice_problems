61699d7ea27a55e4986bf3c80b64e4d0ba5fb55d
wajjih <57046673+wajjih@users.noreply.github.com>
2019-12-18 08:23:32 -0500

    Other Exmples

--- src/basic/OtherExamples.java
+++ src/basic/OtherExamples.java
@@ -9,7 +9,7 @@ public class OtherExamples {
     Expected Output
       2.138888888888889
     */
-
+  double a = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
     // Write a Java program to compute a specified formula.
     /*
     Specified Formula :
@@ -17,7 +17,7 @@ public class OtherExamples {
     Expected Output
     2.9760461760461765
     */
-
+   double b = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
     // Write a Java program to print the area and perimeter of a circle.
     /*
     Test Data:
@@ -26,10 +26,15 @@ public class OtherExamples {
     Perimeter is = 47.12388980384689
     Area is = 176.71458676442586
     */
-
+   double Radius = 7.5;
+     double p = Radius * 2 * 3.14; 
+     double Area = 3.14 * Radius * Radius;
     // Write a Java program that takes three numbers as input to calculate and print the average of the numbers.
-
-
+    int[] arr = {3,6,9};
+    int total = 0;
+    for (int i = 0; i< arr.length; i++)
+    total += arr[i];
+        int average = total / arr.length;
     // Write a Java program to print the area and perimeter of a rectangle.
     /*
     Test Data:
@@ -39,6 +44,10 @@ public class OtherExamples {
       Area is 5.6 * 8.5 = 47.60
       Perimeter is 2 * (5.6 + 8.5) = 28.20
     */
+    double W = 5.5;
+    double H = 8.5;
+    double Aa = W * H;
+    double Perimeter = 2 * (W + H);
 
     // Write a Java program to print an American flag on the screen.
     /*
@@ -62,6 +71,33 @@ public class OtherExamples {
     */
 
 
+
+
     // Write a Java program to swap two variables.
+
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(p);
+    System.out.println(Area);
+    System.out.println(average);
+    System.out.println(Aa);
+    System.out.println(Perimeter);
+    System.out.println("* * * * * * ==================================");
+    System.out.println(" * * * * *  ==================================");
+    System.out.println("* * * * * * ==================================");
+    System.out.println(" * * * * *  ==================================");
+    System.out.println("* * * * * * ==================================");
+    System.out.println(" * * * * *  ==================================");
+    System.out.println("* * * * * * ==================================");
+    System.out.println(" * * * * *  ==================================");
+    System.out.println("* * * * * * ==================================");
+    System.out.println("==============================================");
+    System.out.println("==============================================");
+    System.out.println("==============================================");
+    System.out.println("==============================================");
+    System.out.println("==============================================");
+
+
   }
 }

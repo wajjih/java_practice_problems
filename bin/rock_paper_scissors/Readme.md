15d9af3648e4873dcf2e7644ad0e62017986817b
Wajjih Muhammad <57046673+wajjih@users.noreply.github.com>
2019-12-21 14:10:14 -0500

    You know the vibes

---
+++ bin/rock_paper_scissors/Readme.md
@@ -0,0 +1,44 @@
+# Rock Paper Scissors Game
+Create a rock paper sciessors text game.
+
+## Game Version 1
+
+### Expectations
+step 1: Intro Text
+
+example
+```
+Welcome to Stephon's Rock Paper Scissors Game!
+Enter an option to play.
+( rock, paper, scissors )
+```
+
+step 2: Randomly generate a computers value, then print inputs
+
+example
+```
+user: rock
+computer: scissors
+```
+
+step 3: print the results
+
+example
+```
+You win!
+```
+
+step 4: Ask user if they should want to play again. If yes restart, if no tereminate.
+
+### Implementation
+RockyV1.java ->
+
+> Do everything in static functions or just the main function.
+
+
+## Game Veersion 2
+
+### complete these examples first
+> structs or classes as input to function
+
+Sate share.
--- src/basic/IfStatements.java
+++ src/basic/IfStatements.java
@@ -1,5 +1,5 @@
 package basic;
-
+import java.util.Scanner;
 public class IfStatements {
   public static void main(String[] args) throws Exception {
     // 1. Get number as input, print whether the number is greater than 2
@@ -8,8 +8,14 @@ public class IfStatements {
       2
     Expected Output:
       false
+      */
+        
+      /*int x =7;
+    if (x>2){
+      System.out.println("True");
+      
+    }
     */
-
     // 2. Get number as input, print whether the number is even
     /*
     Test Inputs:
@@ -19,7 +25,17 @@ public class IfStatements {
       true
       false
     */
-
+    /*double x = 2;
+    double y = 3;
+    if (x % 2 == 0){
+      System.out.println("true");
+    }
+    if (y % 2 == 0){
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
+    */
     // 3. Get 2 numbers as input, print whether the 2 numbers are equal to eachother
     /*
     Test Inputs:
@@ -33,5 +49,15 @@ public class IfStatements {
       ...
       true
     */
+    
+    
+    Scanner myObj = new Scanner(System.in);
+    int x = myObj.nextInt() ;
+    int y = myObj.nextInt ();3
+    if ( x == y){
+      System.out.println("true");
+    }else{
+      System.out.println("false");
+    }
   }
 }

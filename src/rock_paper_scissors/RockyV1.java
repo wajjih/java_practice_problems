eeeb62b90aa4dcbe3d4a9a222afa426d283f1d95
wajjih <57046673+wajjih@users.noreply.github.com>
2019-12-25 11:33:52 -0500

    Having Issues :(

--- src/rock_paper_scissors/RockyV1.java
+++ src/rock_paper_scissors/RockyV1.java
@@ -1,9 +1,9 @@
 package rock_paper_scissors;
 
-import java.lang.Math;
+//import java.lang.Math;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.*;
+//import java.util.*;
 
 
 
@@ -18,10 +18,37 @@ public class RockyV1{
 
     //Will create the Scanner and how to input those values to following destinations
     Scanner myobj = new Scanner(System.in);
-            int R = 1;
-            int P = 2;
-            int S = 3;
-      String Response = myobj.nextLine(); 
+            String Rock = "1";
+            String Paper = "2";
+            String Scissors = "3";
+      String Response = myobj.next();
+      Random answer = new Random(); 
+      int Random = answer.nextInt(4); 
+    
+      if (Random == ){
+        System.out.println("Draw");
+      }  
+      if (Random == 1 && Response == Paper){
+        System.out.println("You lose");
+        }else{
+        System.out.println("You win");
+      
+        if (Random == 2 && Response == Scissors){
+          System.out.println("You lose");
+          }else{
+          System.out.println("You win");
+         
+          if (Random == 3 && Response == Rock){
+            System.out.println("You lose");
+            }else{
+            System.out.println("You win");
+      } 
+    } 
+      
+      } 
+      
+
+
         
         
 
